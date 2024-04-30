d56ba33f5c79f950719ee0f0498e34b541d652c7
Davi Siqueira <davicamposd11@gmail.com>
2024-04-30 01:05:53 -0300

    #back-end-develop - Primeira iteração Login

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 //imports
 const express = require("express");
-const sequelize = require ("sequelize");
+const sequelize = require("sequelize");
 const app = express();
 const itemPerdido = require("./Model/ItemPerdido");
+const administrador = require("./admin/Administrador");
 const bodyParser = require("body-parser");
+const adminController = require("./admin/AdminController")
+const session = require("express-sessions")
 
 //configs
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use('/', adminController)
 
 //rotas
-app.get('/', (req, res) => {
-    res.render('formularioPerda');
+app.get("/", (req, res) => {
+  
+  itemPerdido.findAll().then(itens =>{
+
+    res.render("home",{itens:itens})
 })
+ 
+});
 
-app.post('/confirmar', (req, res) => {
+app.post("/logar", (req, res) => {
+  res.render("login");
+});
 
+app.get("/reportarItem", (req, res) => {
+  res.render("formularioPerda");
+});
+
+app.post('/autenticar', (req, res)=>{
+  var username = req.body.username;
+  var password = req.body.password;
+ 
+  
+});
+
+app.post("/confirmar", (req, res) => {
   var nome = req.body.nome;
   var tituloItem = req.body.tituloItem;
   var email = req.body.email;
@@ -31,29 +53,40 @@ app.post('/confirmar', (req, res) => {
   var local = req.body.local;
   var dataPerda = req.body.dataPerda;
 
-  itemPerdido.create({
-    nomePessoa: nome,
-    tituloItem: tituloItem,
-    email: email,
-    descricao: descricao,
-    categoria: categoria,
-    curso: curso,
-    periodo: periodo,
-    cor: cor,
-    local: local,
-    data: dataPerda,
-    marca: marca 
-}).then( res.redirect("/"))
+  itemPerdido
+    .create({
+      nomePessoa: nome,
+      tituloItem: tituloItem,
+      email: email,
+      descricao: descricao,
+      categoria: categoria,
+      curso: curso,
+      periodo: periodo,
+      cor: cor,
+      local: local,
+      data: dataPerda,
+      marca: marca,
+    })
+    .then(res.redirect("/"));
+});
 
-})
-
-
-itemPerdido.sync({ force: false }) // Cria as tabelas se não existirem (force: true)
+//sincronizando banco de dados com o ORM
+administrador
+  .sync({ force: false }) // Cria as tabelas se não existirem (force: true)
   .then(() => {
-    console.log('Tabelas criadas com sucesso.');
+    console.log("Tabelas criadas com sucesso.");
   })
-  .catch(err => {
-    console.error('Erro ao criar tabelas:', err);
+  .catch((err) => {
+    console.error("Erro ao criar tabelas:", err);
+  });
+
+itemPerdido
+  .sync({ force: false }) // Cria as tabelas se não existirem (force: true)
+  .then(() => {
+    console.log("Tabelas criadas com sucesso.");
+  })
+  .catch((err) => {
+    console.error("Erro ao criar tabelas:", err);
   });
 
 app.listen("8083");
