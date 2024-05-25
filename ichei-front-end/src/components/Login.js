18cc1de1042485e6297ad286ff5b72cc038ee713
Diego <diegocdsantos@gmail.com>
2024-05-25 16:43:58 -0300

    pulled the current project

--- ichei-front-end/src/components/Login.js
+++ ichei-front-end/src/components/Login.js
@@ -1,10 +1,49 @@
-function Login(){
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        // Aqui você pode adicionar a lógica para validar o login
+        // e redirecionar o usuário para a página apropriada.
+    };
+
     return (
         <div>
-            <p>Login</p>
-        </div>
-    )
-}
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    <label>Usuário</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Senha</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <button type="submit">Entrar</button>
+                </div>
+            </form>
 
+            <div>
+                <button>Esqueci a Minha Senha</button>
+                <button>FAQ</button>
+            </div>
+        </div>
+    );
+}
 
 export default Login;
--- ichei-front-end/src/components/Main.js
+++ ichei-front-end/src/components/Main.js
@@ -4,15 +4,24 @@ const Main = ({ objects }) => {
         <div>
             <button type="button" disabled>Lista</button>
             <button type="button" disabled>Mapa</button>
-            <ul>
-                {objects.map((objects, index) => (
-                    <li key={index}>
-                        <span>Objeto: {objects.name}</span>
-                        <span>Local: {objects.local}</span>
-                        <span>Data: {objects.data}</span>
-                    </li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Objeto</th>
+                        <th>Local</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {objects.map((object, index) => (
+                        <tr key={index}>
+                            <td>{object.name}</td>
+                            <td>{object.local}</td>
+                            <td>{object.data}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
 
     );
